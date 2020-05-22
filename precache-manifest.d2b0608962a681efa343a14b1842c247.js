d1373dc218f568de48ec723e41a1efa3bab8518a
Kirill <Lequeston@yandex.ru>
2020-05-22 20:32:12 +0300

    first commit

---
+++ precache-manifest.d2b0608962a681efa343a14b1842c247.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5da5166f819deaf5bcb758f023567c53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b0e840ab8be3c1820af",
+    "url": "/static/css/main.bd4cde21.chunk.css"
+  },
+  {
+    "revision": "3f95b1ba4a030bd5c045",
+    "url": "/static/js/2.66fb598f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.66fb598f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b0e840ab8be3c1820af",
+    "url": "/static/js/main.40e430cf.chunk.js"
+  },
+  {
+    "revision": "eafb66cdd8dd26081ad2",
+    "url": "/static/js/runtime-main.8099dc88.js"
+  }
+]);
